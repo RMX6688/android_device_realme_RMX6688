7806224867a0a7d920af4b1177c0d0462e1d463d
StaticReflection <deng3616888303@gmail.com>
2026-02-11 02:26:21 +0800

    RMX6688: Build android.hidl.allocator@1.0 vendor variant

--- device.mk
+++ device.mk
@@ -80,7 +80,8 @@ PRODUCT_PACKAGES += \
     libhidltransport \
     libhidltransport.vendor \
     libhwbinder \
-    libhwbinder.vendor
+    libhwbinder.vendor \
+    android.hidl.allocator@1.0.vendor
 
 # RIL
 PRODUCT_PACKAGES += \
