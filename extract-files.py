431006b35f5339101224838fe25e92bbd0758dd2
StaticReflection <deng3616888303@gmail.com>
2026-02-11 02:07:54 +0800

    RMX6688: Configure Display stack

--- extract-files.py
+++ extract-files.py
@@ -4,7 +4,13 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+
 from extract_utils.fixups_lib import (
+    lib_fixup_remove,
     lib_fixups,
     lib_fixups_user_type,
 )
@@ -21,6 +27,15 @@ lib_fixups: lib_fixups_user_type = {
     **lib_fixups,
     (
     ): lib_fixup_vendor_suffix,
+    (
+        'android.hardware.graphics.allocator-V1-ndk',
+        'android.hardware.graphics.composer3-V2-ndk',
+    ): lib_fixup_remove,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    ('vendor/lib64/hw/hwcomposer.mtk_common.so'): blob_fixup()
+        .add_needed('libprocessgroup_shim.so'),
 }
 
 module = ExtractUtilsModule(
